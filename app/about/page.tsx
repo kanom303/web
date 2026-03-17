8f63fe2d866840cc3c8b7d1702183a828fcc5086
kanom303 <kanom303.knm@gmail.com>
2026-03-18 00:03:57 +0700

    Delete app directory

--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-// ไฟล์: app/about/page.tsx
-export default function AboutPage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">About Us</h1>
-    </div>
-  );
-}
--- app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-// ไฟล์: app/about/page.tsx
-export default function ContactPage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">contact</h1>
-    </div>
-  );
-}
